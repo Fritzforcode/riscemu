e18ef0ab3fcb981c57b63595d3fd53b575a0b2e1
Fritzforcode <88397711+Fritzforcode@users.noreply.github.com>
2025-03-17 19:39:14 +0100

    patched author, copyright and version

---
+++ riscemu/acv.py
@@ -0,0 +1,6 @@
+# to read package version:
+import importlib.metadata
+
+__author__ = "Anton Lydike <Anton@Lydike.com>"
+__copyright__ = "Copyright 2023 Anton Lydike"
+__version__ = importlib.metadata.version("riscemu")
